cbb7ad2ca8d8fa3de9a54285c0d9620961d315c8
amit-agrawal10 <amit.agrawal10@gmail.com>
2020-09-17 07:49:37 +0530

    Initial checkin

---
+++ src/main/java/guru/springframework/microinventoryfailover/MicroInventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.microinventoryfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroInventoryFailoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroInventoryFailoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/microinventoryfailover/MicroInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.microinventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroInventoryFailoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
